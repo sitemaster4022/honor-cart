26397d6a1c088083a5e7eacd2fa7ce1e5237e1b8
sitemaster4022 <sitemaster4022@gmail.com>
2026-09-03 09:29:35 -0400

    Generate canonical sitemap.xml endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://honorcart.com",
-	integrations: [sitemap()],
 	adapter: cloudflare({
 		imageService: "compile",
 	}),
